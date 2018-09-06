4b7843617f641e7681e47c4127026c1042fd786d
Mr. Hezekiah <giwaisaac@hotmail.com>
2018-09-06 14:37:58 +0200

    Created a SingleProject.jsx file

---
+++ src/views/SingleProject.jsx
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react';
+import {Badge} from 'reactstrap';
+import PRojects from '../project.json';
+
+class SingleProject extends React.Component{
+  componentDidMount(){
+    document.title = ''
+  }
+}
